4a6ab9ae4be9ddf82299d03ab40356a78dae7dab
Sviatlana <b_s_s@tut.by>
2021-06-17 17:44:15 +0300

    Homework2

--- src/Homework2/Task7.py
+++ src/Homework2/Task7.py
@@ -3,8 +3,8 @@ side2 = int(input('Введите сторону b:\n'))
 side3 = int(input('Введите сторону c:\n'))
 
 if side1 + side2 > side3 and side1 + side3 > side2 and side3 + side2 > side1:
-    perim = (side1 + side2 + side3) / 2
-    area_tr = (perim * (perim - side1) * (perim - side2) * (perim - side3)) ** 0.5
+    p = (side1 + side2 + side3) / 2
+    area_tr = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
     print('Это треугольник!\n')
     print('Площадь этого треугольника:', area_tr)
 else:
