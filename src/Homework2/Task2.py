d8a9281af985a49ef55eeaf961820734573bb2a0
Sviatlana <b_s_s@tut.by>
2021-06-17 17:25:44 +0300

    Homework2

--- src/Homework2/Task2.py
+++ src/Homework2/Task2.py
@@ -1,3 +1,3 @@
 str1 = input('Введите предложение\n')
-long_word = max(str1.split(), key = len)
+long_word = max(str1.split(), key=len)
 print("Самое длинное слово в предложении: ", long_word)
--- src/Homework2/Task7.py
+++ src/Homework2/Task7.py
@@ -3,8 +3,8 @@ side2 = int(input('Введите сторону b:\n'))
 side3 = int(input('Введите сторону c:\n'))
 
 if side1 + side2 > side3 and side1 + side3 > side2 and side3 + side2 > side1:
-    perimeter = (side1 + side2 + side3) / 2
-    area_tr = (perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3)) ** 0.5
+    perim = (side1 + side2 + side3) / 2
+    area_tr = (perim * (perim - side1) * (perim - side2) * (perim - side3)) ** 0.5
     print('Это треугольник!\n')
     print('Площадь этого треугольника:', area_tr)
 else:
