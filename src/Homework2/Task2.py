ebada1c6215fa445f5f421a884873f2ade261a19
SvBern <82230455+SvBern@users.noreply.github.com>
2021-06-29 02:45:05 +0300

    Delete src/Homework2 directory

--- src/Homework2/Task2.py
+++
@@ -1,3 +0,0 @@
-str1 = input('Введите предложение\n')
-long_word = max(str1.split(), key=len)
-print("Самое длинное слово в предложении: ", long_word)
--- src/Homework2/Task3.py
+++
@@ -1,8 +0,0 @@
-str1 = input('Введите любую строку:')
-
-new_str = ''
-for element in str1:
-    if element not in new_str and element != ' ':
-        new_str += element
-
-print(new_str)
