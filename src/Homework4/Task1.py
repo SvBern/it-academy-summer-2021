d1551a6cd4bede5a9a22e43f37bdf661e9586650
Sviatlana Bernatskaya <b_s_s@tut.by>
2021-07-06 02:53:17 +0300

    Corrected Homework4

--- src/Homework4/Task1.py
+++ src/Homework4/Task1.py
@@ -1,7 +1,8 @@
-# Dict comprehensions
-# Создайте словарь с помощью генератора словарей,
-# так чтобы его ключами были числа от 1 до 20,
-# а значениями кубы этих чисел.
+"""Dict comprehensions
 
-MyDictionary = {i: i**3 for i in range(1, 21)}
-print(MyDictionary)
+Создайте словарь с помощью генератора словарей,
+так чтобы его ключами были числа от 1 до 20,
+а значениями кубы этих чисел.
+"""
+dict_a_cubed = {item: item**3 for item in range(1, 21)}
+print(dict_a_cubed)
--- src/Homework4/Task2.py
+++ src/Homework4/Task2.py
@@ -1,41 +1,43 @@
-# Города
-# Дан список стран и городов каждой страны. Затем даны названия городов.
-# Для каждого города укажите, в какой стране он находится.
-# Входные данные
-# Программа получает на вход количество стран N.
-# Далее идет N строк, каждая строка начинается с названия страны,
-# затем идут названия городов этой страны.
-# В следующей строке записано число M,
-# далее идут M запросов — названия каких-то M городов, перечисленных выше.
-# Выходные данные
-# Для каждого из запроса выведите название страны,
-# в котором находится данный город.
-# Примеры
-# Входные данные
-# 2
-# Russia Moscow Petersburg Novgorod Kaluga
-# Ukraine Kiev Donetsk Odessa
-# 3
-# Odessa
-# Moscow
-# Novgorod
-# Выходные данные
-# Ukraine
-# Russia
-# Russia
+""" Города.
 
+Дан список стран и городов каждой страны. Затем даны названия городов.
+Для каждого города укажите, в какой стране он находится.
+Входные данные:
+Программа получает на вход количество стран N.
+Далее идет N строк, каждая строка начинается с названия страны,
+затем идут названия городов этой страны.
+В следующей строке записано число M,
+далее идут M запросов — названия каких-то M городов, перечисленных выше.
+Выходные данные:
+Для каждого из запроса выведите название страны,
+в котором находится данный город.
+Примеры:
+Входные данные
+2
+Russia Moscow Petersburg Novgorod Kaluga
+Ukraine Kiev Donetsk Odessa
+3
+Odessa
+Moscow
+Novgorod
+Выходные данные
+Ukraine
+Russia
+Russia
+"""
 count_country = int(input('Введите количество стран: \n'))
 
 country_city_dict = {}
-for counter in range(1, count_country + 1):
+for counter in range(count_country):
     country = input('Введите через пробелы страну и её города: \n').split()
-    country_city_dict |= dict.fromkeys(country[1:], country[0])
+    country_city_dict[country[0]] = country[1:]
 
 count_city = int(input('Введите количество проверяемых городов: \n'))
 
-country_list = ""
-for counter in range(1, count_city + 1):
+city_list = []
+for counter in range(count_city):
     name_city = input('Введите название города:  \n')
-    country_list += country_city_dict[name_city] + '\n'
-
-print(country_list)
+    city_list.append(name_city)
+    for key, value in country_city_dict.items():
+        if city_list[counter] in value:
+            print(key)
