59ab28f40eeefe68fab42ff03e9cb303c013a9e7
Sviatlana Bernatskaya <b_s_s@tut.by>
2021-07-23 17:23:34 +0300

    Corrected Task2 after test-flake8

--- src/Homework5/Task2/Task2.py
+++ src/Homework5/Task2/Task2.py
@@ -1,9 +1,9 @@
 def storage_of_results_decor(func):
-    """Создайте декоратор,
+    """
+    Создайте декоратор,
     который хранит результаты вызовов функции
     (за все время вызовов, не только текущий запуск программы)
     """
-
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         with open("all_results.txt", "a") as all_results:
